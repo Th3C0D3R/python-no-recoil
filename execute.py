d73eb39acb6fad9db8e07333b07a429769d924f0
Th3OnlyC0D3R <nico.fengler@gmx.de>
2018-08-16 16:28:49 +0200

    Add files via upload

--- execute.py
+++ execute.py
@@ -5,19 +5,22 @@ import random
 from random import randint
 import dontExecute
 import os, time, sys
+from threading import Timer
 
 # vars
 rcson = False
 procfound = False
-rcsactive = False
+rapidFire = False
 rndopt = randint(0,1)
-toggleKey, exitKey, reloadKey, restartKey, upKey, downKey, leftKey, rightKey, page_up, page_down, activeSlot, slot1, slot2, slot3, aPid, count = (0,)*16
+lastKey, toggleKey, exitKey, reloadKey, restartKey, upKey, downKey, leftKey, rightKey, page_up, page_down, rapidFireKey, activeSlot, slot1, slot2, slot3, aPid, count = (0,)*18
 nPid = 1
-searchname = "TslGame" #PUBG = TlsGame
+processname = "notepad++" #PUBG = TslGame
 error_code = [dontExecute.bcolors.ENDC,""]
+somethingChanged = False
+gameSelected = False
 
 def loadConfig():
-	global toggleKey, exitKey, reloadKey,restartKey, upKey, downKey, leftKey, rightKey, page_up, page_down, configLoaded, slot1, slot2, slot3
+	global toggleKey, exitKey, reloadKey,restartKey, upKey, downKey, leftKey, rightKey, page_up, page_down,processname, rapidFireKey, configLoaded, slot1, slot2, slot3, somethingChanged
 	toggleKey = int(dontExecute.getConfig("Settings","default_toggle_key"))
 	exitKey = int(dontExecute.getConfig("Settings","default_exit_key"))
 	reloadKey = int(dontExecute.getConfig("Settings","default_reload_key"))
@@ -28,51 +31,48 @@ def loadConfig():
 	rightKey = int(dontExecute.getConfig("Settings","default_select_key_right"))
 	page_up = int(dontExecute.getConfig("Settings","default_select_key_second_up"))
 	page_down = int(dontExecute.getConfig("Settings","default_select_key_second_down"))
+	rapidFireKey = int(dontExecute.getConfig("Settings","default_rapidFireKey"))
+	processname = str(dontExecute.getConfig("Settings","default_processname"))
 	slot1 = str(dontExecute.getConfig("Settings","slot1"))
 	slot2 = str(dontExecute.getConfig("Settings","slot2"))
 	slot3 = str(dontExecute.getConfig("Settings","slot3"))
+	somethingChanged = True
 	
 def drawScreen():
-	os.system('cls' if os.name=='nt' else 'clear')
-	outStr  = "============No-Recoil-Script============\n"
-	outStr += "\n"
-	outStr += "Process found: " + str(procfound)+"\n"
-	outStr += "Game selected: " + str(aPid == nPid)+"\n"
-	outStr += "Recoil-Script: " + (str(True) if rcson else str(False))+"\n"
-	outStr += "RCS Active   : " + (str(True) if rcsactive else str(False))+"\n"
-	outStr += "\n"
-	outStr += "Keybinds:\n"
-	outStr += "Toggle Recoil-Script	: " + dontExecute.getKeyFromValue(toggleKey)+"\n"
-	outStr += "Change Weapon 	   	: " + dontExecute.getKeyFromValue(leftKey)+" and "+ dontExecute.getKeyFromValue(rightKey)+"\n"
-	outStr += "for current Slot\n"
-	outStr += "Lower Recoil 	   	: " + dontExecute.getKeyFromValue(downKey)+"\n"
-	outStr += "Higher Recoil 	   	: " + dontExecute.getKeyFromValue(upKey)+"\n"
-	outStr += "Lower Second Recoil 	: " + dontExecute.getKeyFromValue(page_up)+"\n"
-	outStr += "Higher Second Recoil	: " + dontExecute.getKeyFromValue(page_down)+"\n"
-	outStr += "Select Slot 1		: Numpad 1 or 1\n"
-	outStr += "Select Slot 2		: Numpad 2 or 3\n"
-	outStr += "Select Slot 3		: Numpad 2 or 3\n"
-	outStr += "Exit Recoil-Script  	: " + dontExecute.getKeyFromValue(exitKey)+"\n"
-	outStr += "Reload Config       	: " + dontExecute.getKeyFromValue(reloadKey)+"\n"
-	outStr += "Restart Script      	: " + dontExecute.getKeyFromValue(restartKey)+"\n"
-	outStr += "\n"
-	outStr += "\n"
-	outStr += "Weapon Slot 1:    	" + slot1 + ("       Active " if activeSlot==1 else "" )+"\n"
-	outStr += "Recoil-Value:		" + str(dontExecute.getRecoilValues(slot1)[0])+"\n"
-	outStr += "Recoil-Value Second:	" + str(dontExecute.getRecoilValues(slot1)[1])+"\n"
-	outStr += "\n"
-	outStr += "Weapon Slot 2:    	" + slot2 + ("       Active " if activeSlot==2 else "" )+"\n"
-	outStr += "Recoil-Value:     	" + str(dontExecute.getRecoilValues(slot2)[0])+"\n"
-	outStr += "Recoil-Value Second:	" + str(dontExecute.getRecoilValues(slot2)[1])+"\n"
-	outStr += "\n"
-	outStr += "Weapon Slot 3:    	" + slot3 + ("       Active " if activeSlot==3 else "" )+"\n"
-	outStr += "Recoil-Value:     	" + str(dontExecute.getRecoilValues(slot3)[0])+"\n"
-	outStr += "Recoil-Value Second:	" + str(dontExecute.getRecoilValues(slot3)[1])+"\n"
-	outStr += "\n"
-	outStr += error_code[0] + error_code[1] + dontExecute.bcolors.ENDC+"\n"
-	outStr += "\n"
-	outStr += "========================================\n"
-	print(outStr)
+	global somethingChanged
+	if somethingChanged:
+		os.system('cls' if os.name=='nt' else 'clear')
+		outStr  = "========================= "+ dontExecute.bcolors.HEADER +" No-Recoil-Script V2.2 "+ dontExecute.bcolors.ENDC +" =========================\n"
+		outStr += "\n"
+		outStr += dontExecute.bcolors.YELLOW + "Status:"+ dontExecute.bcolors.ENDC+"\n"
+		outStr += "Process found: " + "{:<5}".format(((dontExecute.bcolors.BLUE + str(True)) if procfound else (dontExecute.bcolors.RED + str(False)))) + dontExecute.bcolors.ENDC+   "       Recoil-Script: " + "{:<5}".format(((dontExecute.bcolors.BLUE + str(True)) if rcson else (dontExecute.bcolors.RED + str(False)))) + dontExecute.bcolors.ENDC+"\n"
+		outStr += "Game selected: " + "{:<5}".format(((dontExecute.bcolors.BLUE + str(True)) if aPid==nPid else (dontExecute.bcolors.RED + str(False)))) + dontExecute.bcolors.ENDC+"       Rapidfire    : " + "{:<5}".format(((dontExecute.bcolors.BLUE + str(True)) if rapidFire else (dontExecute.bcolors.RED + str(False)))) + dontExecute.bcolors.ENDC+"\n"
+		outStr += "\n"
+		outStr += "\n"
+		outStr += dontExecute.bcolors.YELLOW + "Weapon Selection:" + dontExecute.bcolors.ENDC+"\n"
+		outStr += (dontExecute.bcolors.GREEN if activeSlot==1 else dontExecute.bcolors.ENDC ) + "Weapon Slot 1:    	" + "{:<8}".format(slot1) + dontExecute.bcolors.ENDC + (dontExecute.bcolors.GREEN if activeSlot==2 else dontExecute.bcolors.ENDC ) + "       Weapon Slot 2:    	" + slot2 + dontExecute.bcolors.ENDC+"\n"
+		outStr += (dontExecute.bcolors.GREEN if activeSlot==1 else dontExecute.bcolors.ENDC ) + "Recoil-Value:		" + str(dontExecute.getRecoilValues(slot1)[0])+ dontExecute.bcolors.ENDC + (dontExecute.bcolors.GREEN if activeSlot==2 else dontExecute.bcolors.ENDC ) +"             Recoil-Value:     	" + str(dontExecute.getRecoilValues(slot2)[0]) + dontExecute.bcolors.ENDC+"\n"
+		outStr += (dontExecute.bcolors.GREEN if activeSlot==1 else dontExecute.bcolors.ENDC ) + "Recoil-Value Second:	" + str(dontExecute.getRecoilValues(slot1)[1])+ dontExecute.bcolors.ENDC + (dontExecute.bcolors.GREEN if activeSlot==2 else dontExecute.bcolors.ENDC ) +"             Recoil-Value Second:	" + str(dontExecute.getRecoilValues(slot2)[1]) + dontExecute.bcolors.ENDC+"\n"
+		outStr += "\n"
+		outStr += "\n"
+		outStr += dontExecute.bcolors.YELLOW + "Keybinds:" + dontExecute.bcolors.ENDC+"\n"
+		outStr += "Toggle Recoil-Script	: " + dontExecute.getKeyFromValue(toggleKey)+"\n"
+		outStr += "Toggle Rapidfire 	: " + dontExecute.getKeyFromValue(rapidFireKey)+"\n"
+		outStr += "Change Weapon 	   	: " + dontExecute.getKeyFromValue(leftKey)+" & "+ dontExecute.getKeyFromValue(rightKey)+"\n"
+		outStr += "Change Recoil 	   	: " + dontExecute.getKeyFromValue(upKey)+" & "+ dontExecute.getKeyFromValue(downKey)+"\n"
+		outStr += "Change Second Recoil	: " + dontExecute.getKeyFromValue(page_up)+" & "+ dontExecute.getKeyFromValue(page_down)+"\n"
+		outStr += "Select Slot  		: 1/Numpad 1    &    2/Numpad 2\n"
+		outStr += "Reload Config       	: " + dontExecute.getKeyFromValue(reloadKey)+"\n"
+		outStr += "Restart Script      	: " + dontExecute.getKeyFromValue(restartKey)+"\n"
+		outStr += "Exit Recoil-Script  	: " + dontExecute.getKeyFromValue(exitKey)+"\n"
+		outStr += "\n"
+		outStr += "Config File Path: " + os.path.realpath('config.cfg') + "\n"
+		outStr += "\n"
+		outStr += error_code[0] + error_code[1] + dontExecute.bcolors.ENDC+"\n"
+		outStr += "\n"
+		outStr += "=========================== "+ dontExecute.bcolors.HEADER +" Made by SiedlerLP "+ dontExecute.bcolors.ENDC +" ===========================\n"
+		print(outStr)
+		somethingChanged = False
 
 	
 def restart():
@@ -90,159 +90,187 @@ else:
 		print(" And run the script again to create a new config file")
 		print(" after that you can update your changes from the old config file to the new one")
 		exit()
+
+loadConfig()
 	
 while True:
-	loadConfig()
 	drawScreen()
+	
 	if procfound:
-		if dontExecute.processExists(searchname) == False:
+		if dontExecute.processExists(processname) == False:
 			procfound = False
+			gameSelected = False
+			rapidFire = False
+			aPid = 0
+			nPid = 1
+			somethingChanged = True
 		else:
-			aPid = int(dontExecute.activeWindow())
-			nPid = int(dontExecute.neededWindow(searchname))		
+			try:
+				aPid = int(dontExecute.activeWindow())
+				nPid = int(dontExecute.neededWindow(processname))
+			except:
+				aPid = 0
+				nPid = 1
 			if (aPid == nPid):
-				error_code = [dontExecute.bcolors.ENDC,""]
+				if gameSelected == False:
+					somethingChanged = True
+					gameSelected = True
 				if win32api.GetAsyncKeyState(toggleKey):
 					error_code = [dontExecute.bcolors.ENDC,""]
 					rcson = not rcson
 					sleep(0.2)
-					error_code = [dontExecute.bcolors.OKBLUE,"No-Recoil: Activate"]
+					error_code = [dontExecute.bcolors.YELLOW,"No-Recoil: Activate"]
+					somethingChanged = True
 				if win32api.GetAsyncKeyState(exitKey):
 					error_code = [dontExecute.bcolors.ENDC,""]
+					somethingChanged = True
 					print('RCS SHUTTING DOWN')
 					sleep(1)
 					os.system('cls')
 					exit()
+				if win32api.GetAsyncKeyState(rapidFireKey):
+					rapidFire = not rapidFire
+					somethingChanged = True
 				while rcson is True:
-					count = 0
-					drawScreen()
-					while mouse.is_pressed(button='left'):
-						rcsactive = True
-						if count < 40:
-							rndopt = randint(0,1)
-							if rndopt == 1:
-								randomized = randint(0,5)
-							elif rndopt == 0:
-								randomized = random.uniform(0,-5)    
-							if count <= 11 or count > 21:
+					try:
+						aPid = int(dontExecute.activeWindow())
+						nPid = int(dontExecute.neededWindow(processname))
+					except:
+						aPid = 0
+						nPid = 1
+					if aPid != nPid:
+						gameSelected = False
+						rapidFire = False
+						rcson = False
+						aPid = 0
+						nPid = 1
+						somethingChanged = True
+					else:
+						count = 0
+						drawScreen()
+						while mouse.is_pressed(button='left'):
+							if rapidFire:
+								mouse.release(button='left')
+								mouse.press(button="left")
+							if activeSlot != 0:
+								rndopt = randint(0,1)
+								if rndopt == 1:
+									randomized = randint(0,5)
+								elif rndopt == 0:
+									randomized = random.uniform(0,-5)    
+								if count <= 11 or count > 21:
+								
+									ammount = int(dontExecute.getRecoilValues((slot1 if activeSlot==1 else (slot2 if activeSlot==2 else slot1)))[0])
+								else:
+									ammount = int(dontExecute.getRecoilValues((slot1 if activeSlot==1 else (slot2 if activeSlot==2 else slot1)))[1])
+								
+								if rndopt == 1:
+									ammount = ammount + randomized
+								elif rndopt == 0:
+									ammount = ammount - randomized
+								ammountFinal = int(round(ammount))
+								win32api.mouse_event(0x0001,0,ammountFinal)
+								count = count + 1
+							sleep(0.1) 
 							
-								ammount = int(dontExecute.getRecoilValues((slot1 if activeSlot==1 else (slot2 if activeSlot==2 else (slot3 if activeSlot==3 else slot1))))[0])
-							else:
-								ammount = int(dontExecute.getRecoilValues((slot1 if activeSlot==1 else (slot2 if activeSlot==2 else (slot3 if activeSlot==3 else slot1))))[1])
-							
-							if rndopt == 1:
-								ammount = ammount + randomized
-							elif rndopt == 0:
-								ammount = ammount - randomized
-							ammountFinal = int(round(ammount))
-							win32api.mouse_event(0x0001,0,ammountFinal)
-							sleep(0.10)    
-						count = count + 1
-						if count > 40:
-							count = 0
-							ammount = 0  
-					rcsactive = False
-					
 					if win32api.GetAsyncKeyState(toggleKey):
 						error_code = [dontExecute.bcolors.ENDC,""]
 						rcson = not rcson
 						sleep(0.2)
-						error_code = [dontExecute.bcolors.OKBLUE,"No-Recoil: Deactivated"]
+						error_code = [dontExecute.bcolors.YELLOW,"No-Recoil: Deactivated"]
 						activeSlot = 0
+						somethingChanged = True
 						
 					if win32api.GetAsyncKeyState(exitKey):
 						error_code = [dontExecute.bcolors.ENDC,""]
+						somethingChanged = True
 						print('RCS SHUTTING DOWN')
 						sleep(1)
 						os.system('cls')
 						exit()
-
+						
+					if win32api.GetAsyncKeyState(rapidFireKey):
+						rapidFire = not rapidFire
+						somethingChanged = True
+						
 					if win32api.GetAsyncKeyState(dontExecute.getValueFromKey('1')) or win32api.GetAsyncKeyState(dontExecute.getValueFromKey('Numpad 1')):
 						activeSlot = 1
+						somethingChanged = True
 					if win32api.GetAsyncKeyState(dontExecute.getValueFromKey('2')) or win32api.GetAsyncKeyState(dontExecute.getValueFromKey('Numpad 2')):
 						activeSlot = 2
-					if win32api.GetAsyncKeyState(dontExecute.getValueFromKey('3')) or win32api.GetAsyncKeyState(dontExecute.getValueFromKey('Numpad 3')):
-						activeSlot = 3
-						
+						somethingChanged = True
+
 					if win32api.GetAsyncKeyState(leftKey):
 						if activeSlot == 1:
 							slot1 = dontExecute.getNextItem(slot1,True)
 							dontExecute.saveConfig("Settings","slot1", slot1)
+							somethingChanged = True
 						if activeSlot == 2:
 							slot2 = dontExecute.getNextItem(slot2,True)
 							dontExecute.saveConfig("Settings","slot2", slot2)
-						if activeSlot == 3:
-							slot3 = dontExecute.getNextItem(slot3,True)
-							dontExecute.saveConfig("Settings","slot3", slot3)
+							somethingChanged = True
 					if win32api.GetAsyncKeyState(rightKey):
 						if activeSlot == 1:
 							slot1 = dontExecute.getNextItem(slot1,False)
 							dontExecute.saveConfig("Settings","slot1", slot1)
+							somethingChanged = True
 						if activeSlot == 2:
 							slot2 = dontExecute.getNextItem(slot2,False)
 							dontExecute.saveConfig("Settings","slot2", slot2)
-						if activeSlot == 3:
-							slot3 = dontExecute.getNextItem(slot3,False)
-							dontExecute.saveConfig("Settings","slot3", slot3)
+							somethingChanged = True
 							
 					if win32api.GetAsyncKeyState(downKey):
 						if activeSlot == 1:
 							rcsValues = dontExecute.getRecoilValues(slot1)
 							rcsValues[0] = str(int(rcsValues[0]) - 1)
 							dontExecute.saveConfig("Weapons",slot1, ','.join(rcsValues))
+							somethingChanged = True
 						if activeSlot == 2:
 							rcsValues = dontExecute.getRecoilValues(slot2)
 							rcsValues[0] = str(int(rcsValues[0]) - 1)
 							dontExecute.saveConfig("Weapons",slot2, ','.join(rcsValues))
-						if activeSlot == 3:
-							rcsValues = dontExecute.getRecoilValues(slot3)
-							rcsValues[0] = str(int(rcsValues[0]) - 1)
-							dontExecute.saveConfig("Weapons",slot3, ','.join(rcsValues))
+							somethingChanged = True
 					if win32api.GetAsyncKeyState(upKey):
 						if activeSlot == 1:
 							rcsValues = dontExecute.getRecoilValues(slot1)
 							rcsValues[0] = str(int(rcsValues[0]) + 1)
 							dontExecute.saveConfig("Weapons",slot1, ','.join(rcsValues))
+							somethingChanged = True
 						if activeSlot == 2:
 							rcsValues = dontExecute.getRecoilValues(slot2)
 							rcsValues[0] = str(int(rcsValues[0]) + 1)
 							dontExecute.saveConfig("Weapons",slot2, ','.join(rcsValues))
-						if activeSlot == 3:
-							rcsValues = dontExecute.getRecoilValues(slot3)
-							rcsValues[0] = str(int(rcsValues[0]) + 1)
-							dontExecute.saveConfig("Weapons",slot3, ','.join(rcsValues))
+							somethingChanged = True
 							
 					if win32api.GetAsyncKeyState(page_down):
 						if activeSlot == 1:
 							rcsValues = dontExecute.getRecoilValues(slot1)
 							rcsValues[1] = str(int(rcsValues[1]) - 1)
 							dontExecute.saveConfig("Weapons",slot1, ','.join(rcsValues))
+							somethingChanged = True
 						if activeSlot == 2:
 							rcsValues = dontExecute.getRecoilValues(slot2)
 							rcsValues[1] = str(int(rcsValues[1]) - 1)
 							dontExecute.saveConfig("Weapons",slot2, ','.join(rcsValues))
-						if activeSlot == 3:
-							rcsValues = dontExecute.getRecoilValues(slot3)
-							rcsValues[1] = str(int(rcsValues[1]) - 1)
-							dontExecute.saveConfig("Weapons",slot3, ','.join(rcsValues))
+							somethingChanged = True
+
 					if win32api.GetAsyncKeyState(page_up):
 						if activeSlot == 1:
 							rcsValues = dontExecute.getRecoilValues(slot1)
 							rcsValues[1] = str(int(rcsValues[1]) + 1)
 							dontExecute.saveConfig("Weapons",slot1, ','.join(rcsValues))
+							somethingChanged = True
 						if activeSlot == 2:
 							rcsValues = dontExecute.getRecoilValues(slot2)
 							rcsValues[1] = str(int(rcsValues[1]) + 1)
 							dontExecute.saveConfig("Weapons",slot2, ','.join(rcsValues))
-						if activeSlot == 3:
-							rcsValues = dontExecute.getRecoilValues(slot3)
-							rcsValues[1] = str(int(rcsValues[1]) + 1)
-							dontExecute.saveConfig("Weapons",slot3, ','.join(rcsValues))
+							somethingChanged = True
 							
 					if win32api.GetAsyncKeyState(reloadKey):
 						error_code = [dontExecute.bcolors.ENDC,""]
 						loadConfig()
-						error_code = [dontExecute.bcolors.OKGREEN,"Info: Config successfull reloaded"]
+						error_code = [dontExecute.bcolors.GREEN,"Info: Config successfull reloaded"]
+						somethingChanged = True
 						
 					if win32api.GetAsyncKeyState(restartKey):
 						print('RCS RESTARTING IN 2sec')
@@ -250,11 +278,19 @@ while True:
 						restart()
 					sleep(0.1)
 			else:
+				gameSelected = False
+				rapidFire = False
+				rcson = False
+				aPid = 0
+				nPid = 1
+				somethingChanged = True
 				if win32api.GetAsyncKeyState(toggleKey):
 					error_code = [dontExecute.bcolors.ENDC,""]
-					error_code = [dontExecute.bcolors.FAIL,"ERROR: Target Window is not selected"]
+					error_code = [dontExecute.bcolors.RED,"ERROR: Target Window is not selected"]
+					somethingChanged = True
 				if win32api.GetAsyncKeyState(exitKey):
 					error_code = [dontExecute.bcolors.ENDC,""]
+					somethingChanged = True
 					print('RCS SHUTTING DOWN')
 					sleep(1)
 					os.system('cls')
@@ -262,8 +298,9 @@ while True:
 				if win32api.GetAsyncKeyState(reloadKey):
 					error_code = [dontExecute.bcolors.ENDC,""]
 					loadConfig()
-					error_code = [dontExecute.bcolors.OKGREEN,"Info: Config successfull reloaded"]
+					error_code = [dontExecute.bcolors.GREEN,"Info: Config successfull reloaded"]
 					rcson = False
+					somethingChanged = True
 				if win32api.GetAsyncKeyState(restartKey):
 					print('RCS RESTARTING IN 2sec')
 					sleep(2)
@@ -272,12 +309,15 @@ while True:
 		if win32api.GetAsyncKeyState(reloadKey):
 			error_code = [dontExecute.bcolors.ENDC,""]
 			loadConfig()
-			error_code = [dontExecute.bcolors.OKGREEN,"Info: Config successfull reloaded"]
+			error_code = [dontExecute.bcolors.GREEN,"Info: Config successfull reloaded"]
+			somethingChanged = True
 		if win32api.GetAsyncKeyState(toggleKey):
 			error_code = [dontExecute.bcolors.ENDC,""]
-			error_code = [dontExecute.bcolors.FAIL,'ERROR: Target Process is not running']
+			error_code = [dontExecute.bcolors.RED,'ERROR: Target Process is not running']
+			somethingChanged = True
 		if win32api.GetAsyncKeyState(exitKey):
 			error_code = [dontExecute.bcolors.ENDC,""]
+			somethingChanged = True
 			print('RCS SHUTTING DOWN')
 			sleep(1)
 			os.system('cls')
@@ -286,8 +326,9 @@ while True:
 			print('RCS RESTARTING IN 2sec')
 			sleep(2)
 			restart()
-		if dontExecute.processExists(searchname):
+		if dontExecute.processExists(processname):
 			error_code = [dontExecute.bcolors.ENDC,""]
 			procfound = True
+			somethingChanged = True
 		sleep(0.1)
 	sleep(0.1)
